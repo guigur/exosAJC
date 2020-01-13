e5ae6de230caf4b218b0281deabde7785f5c4673
guigur <guigur13@gmail.com>
2020-01-13 12:13:52 +0100

    ex1

--- exosBash/ex1.sh
+++ exosBash/ex1.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
+# guigur 13/01/2020
 
+echo "saisir la note"
+read note
+
+if  [ $note -ge 16 ] && [ $note -le 20 ]
+then
+   echo "très bien"
+elif [ $note -ge 14 ] && [ $note -lt 16 ]
+then
+    echo "bien"
+elif [ $note -ge 12 ] && [ $note -lt 14 ]
+then
+    echo "assez bien"
+elif [ $note -ge 10 ] && [ $note -lt 12 ]
+then
+    echo "moyen"
+elif [ $note -ge 0 ] && [ $note -lt 10 ]
+then
+    echo "insuffisant"    
+fi
